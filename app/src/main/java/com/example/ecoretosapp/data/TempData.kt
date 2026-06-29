ab09c67463af97fe0b7227209bab6895dc5a5421
Jose Cano <jgcano@uamv.edu.ni>
2026-06-28 22:12:41 -0600

    feat: eliminacion de TempData

--- app/src/main/java/com/example/ecoretosapp/data/TempData.kt
+++
@@ -1,9 +0,0 @@
-package com.example.ecoretosapp.data
-
-import com.example.ecoretosapp.data.model.RetoPropuesto
-import com.example.ecoretosapp.data.model.Evidencia
-
-object TempData {
-    val retosPropuestos = mutableListOf<RetoPropuesto>()
-    val evidenciasPendientes = mutableListOf<Evidencia>()
-}
